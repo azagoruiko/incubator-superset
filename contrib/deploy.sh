8d19f5f63e8d80a53c65f11a2ea01aad3b761abd
zeus <a@zagoruiko.org.ua>
2023-01-12 09:50:31 +0100

    Fix dependencies

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -1,6 +1,6 @@
-DOCKER_REPO="192.168.0.21:9999/docker"
+DOCKER_REPO="10.8.0.5:9999/docker"
 VER=0.0.13
-export NOMAD_ADDR="http://192.168.0.21:4646"
+export NOMAD_ADDR="http://10.8.0.1:4646"
 if [ ! -e $1 ]; then
     VER=$1
 fi
@@ -12,4 +12,4 @@ echo $DOCKER_TAG
 docker build -t "$DOCKER_TAG" -f Dockerfile .
 docker push "$DOCKER_TAG"
 
-nomad job run contrib/nomad/superset.nomad
+#nomad job run contrib/nomad/superset.nomad
